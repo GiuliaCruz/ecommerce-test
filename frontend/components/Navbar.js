083b093c597fb7fc87ea0155955de9e3fec9b10e
GiuliaCruz <giuliacruzz@gmail.com>
2023-05-07 16:11:48 -0300

    commit

---
+++ frontend/components/Navbar.js
@@ -0,0 +1,35 @@
+import './Navbar.css';
+
+const NavBar = () => {
+    return(
+        <nav className= "navbar">
+            {/*logo*/}
+             <div className="navbar__logo">
+        <h2>MERN Shopping Cart</h2>
+      </div>
+
+            {/*links*/}
+      <ul className="navbar__links">
+        <li>
+          <Link to="/cart" className="cart__link">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart <span className="cartlogo__badge">{getCartCount()}</span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+      </ul>
+            {/*hamburger menu*/}
+      <div className="hamburger__menu" onClick={click}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+        </nav>
+    )
+}
+
+export default NavBar
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="app">
+      {/*NavBar*/}
+      {/*SideDrawer*/}
+      {/*Backdrop*/}
+      {/*HomeScreen*/}
+      {/*ProductScreen*/}
+      {/*CartScreen*/}
      Hello mate
     </div>
   );
