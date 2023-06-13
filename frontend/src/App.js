863afe584bf5ba25e5f31202d9aa2e4c8734c57a
GiuliaCruz <giuliacruzz@gmail.com>
2023-06-13 17:33:30 -0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-//Screens
+// Screens
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
-//Components
+// Components
 import NavBar from './components/Navbar';
+import Backdrop from './components/Backdrop';
+import SideDrawer from './components/SideDrawer';
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
       <NavBar />
-      {/*SideDrawer*/}
-      {/*Backdrop*/}
+      <SideDrawer show={sideToggle}/>
+      <Backdrop show={sideToggle}/>
       <main>
-        <Switch>
-          <Route exact path ="/" Component={HomeScreen}/>
-          <Route exact path="/product/:id"Component={ProductScreen}/>
-          <Route exact path="/cart"Component={CartScreen}/>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/cart" element={<CartScreen />} />
+        </Routes>
       </main>
-     Hello mate
     </Router>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,35 +1,34 @@
-import './Navbar.css';
-import {Link} from 'react-router-dom'
-const NavBar = () => {
-    return(
-        <nav className= "navbar">
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
-             <div className="navbar__logo">
+const NavBar = () =>{
+  return(
+    <nav className="navbar">
+      <div className="navbar__logo">
         <h2>MERN Shopping Cart</h2>
       </div>
-
-
       <ul className="navbar__links">
         <li>
-          <Link to="/cart" className="cart__link">
+          <Link to = "/cart" className="cart__Link">
             <i className="fas fa-shopping-cart"></i>
             <span>
-              Cart <span className="cartlogo__badge">{getCartCount()}</span>
+            Cart
             </span>
+            <span className="cartlogo__badge">0</span>
           </Link>
         </li>
         <li>
-          <Link to="/">Shop</Link>
+          <Link to = "/">Shop</Link>
         </li>
       </ul>
 
-      <div className="hamburger__menu" onClick={click}>
+      <div className="hamburger__menu">
         <div></div>
         <div></div>
         <div></div>
       </div>
-        </nav>
-    )
+    </nav>
+  )
 }
 
-export default NavBar
+export default NavBar;
