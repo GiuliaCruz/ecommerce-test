bbb6cce1b62b7d8aa717829f21101df929b25db7
GiuliaCruz <giuliacruzz@gmail.com>
2023-07-13 17:07:28 -0300

    commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,22 @@
 import './HomeScreen.css';
+import { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+
 import Product from "../components/Product";
 
+import { getProducts as listProducts} from '../redux/actions/productActions';
+ 
 const HomeScreen = () => {
+
+    const dispach = useDispatch();
+
+    const getProducts = useSelector(state => state.getProducts);
+    const {products, loading, error} = getProducts;
+
+    useEffect(()=> {
+        dispach(listProducts())
+    }, [dispach]) 
+
     return <div className="homescreen">
         <h2 className="homescreen__title">Latest Products</h2>
         
